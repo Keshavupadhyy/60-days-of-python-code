6b9dbdcc0728e3d10fba6d512f0c9c71d3eca1a6
Keshav Upadhyay <109733099+Keshavupadhyy@users.noreply.github.com>
2024-02-27 17:51:44 +0530

    Add files via upload

---
+++ day-3-if-else.py
@@ -0,0 +1,7 @@
+#Conditional statements (if, else, and elif) are fundamental programming constructs that allow you to control the flow of your program based on conditions that you specify.
+print("welcome to the rollercoster")
+height = int(input("What is your height : "))
+if height != 120:
+    print("you can ride rollercoster")
+else:
+    print("you cannot ride the rollercoster")
